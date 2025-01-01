3d9dc0e8a5e5c1b4567412783fe2f0b64ea7710e
poornima <kannayyavaka@gmail.com>
2025-01-01 18:40:09 +0530

    shell script

---
+++ 08-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("devra" "rrr" "mufasa")
+# index starts from 0, size is 3
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[0]}"
