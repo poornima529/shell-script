c01cde4caf17968e66bc4862d874cd599a9d892a
poornima <kannayyavaka@gmail.com>
2025-01-08 21:20:53 +0530

    expense shell

--- 13-backup.sh
+++ 13-backup.sh
@@ -37,13 +37,13 @@ then
     USAGE
 fi 
 
-if [ ! -d $SOURCE_DIR]
+if [ ! -d $SOURCE_DIR ]
 then
     echo -e "$SOURCE_DIR Does not exist...please check"
     exit
 fi
 
-if [ ! -d $DEST_DIR]
+if [ ! -d $DEST_DIR ]
 then
     echo -e "$DEST_DIR Does not exist...please check"
     exit
